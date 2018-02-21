9e51bb36fa664248341b5016c12e4e04551acddc
AjayMukhi <31158461+AjayMukhi@users.noreply.github.com>
2018-02-20 20:25:32 -0600

    Update MainParser.java

--- parser/MainParser.java
+++ parser/MainParser.java
@@ -1,5 +1,10 @@
 package parser;
 
+/** Program to call all parsers and from Main file which run all the xml and load the records into the database
+ *  Programmed by Ajay and Karan 
+ *  for Project SOA -566
+ */
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
